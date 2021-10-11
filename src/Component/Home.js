443c8fbb1fefab2b0e61e3c889bcafe92cf89961
Kapil-Kawathekar <kapilkawathekar8@gmail.com>
2021-10-11 10:41:46 +0530

    Fixed homePage buttons

--- src/Component/Home.js
+++ src/Component/Home.js
@@ -2,37 +2,44 @@ import { Grid, Typography, Button } from "@mui/material";
 import Landing from "../assests/landing.jpg";
 import React from "react";
 import Services from "./Services";
+import { Link } from "react-router-dom";
 
 function Home() {
   return (
     <div>
-      <Grid container spacing={1} alignItems='center'>
+      <Grid container spacing={1} alignItems="center">
         <Grid item xs={6}>
-          <Typography variant='h6'>
+          <Typography variant="h6">
             Looking for All in One Marketing Solution of your Business? You're
             at right place!
           </Typography>
-          <Typography variant='h3' style={{ marginTop: 20 }}>
+          <Typography variant="h3" style={{ marginTop: 20 }}>
             Full Stack Marketing Services
           </Typography>
-          <Typography variant='h6' color='primary' style={{ marginTop: 10 }}>
+          <Typography variant="h6" color="primary" style={{ marginTop: 10 }}>
             One Solution for Influencer to get consistent revenue. One solution
             for Brand to engage with your target audience.
           </Typography>
-          <Grid container alignItems='center' style={{ marginTop: 20 }}>
+          <Grid container alignItems="center" style={{ marginTop: 20 }}>
             <Grid item xs={3}></Grid>
             <Grid item xs={2}>
-              <Button variant='contained'>I'm Brand</Button>
+              <Link to="/brand">
+                {" "}
+                <Button variant="contained">I'M BRAND</Button>
+              </Link>
             </Grid>
             <Grid item xs={3}>
-              <Button variant='contained'>I'm Influencer</Button>
+              <Link to="/influencer">
+                {" "}
+                <Button variant="contained">I'M INFLUENCER</Button>
+              </Link>
             </Grid>
           </Grid>
         </Grid>
         <Grid item xs={6}>
           <img
             src={Landing}
-            alt='landing'
+            alt="landing"
             style={{ width: "80%", height: "60%" }}
           />
         </Grid>
--- src/index.js
+++ src/index.js
@@ -6,10 +6,13 @@ import "antd/dist/antd.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
 );
