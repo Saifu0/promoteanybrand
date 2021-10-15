3c78b690d401d626451e97f94f06c7e60bbf7bb9
Shitanshu Sagar <84727091+Sitanshu-AI@users.noreply.github.com>
2021-10-15 15:17:15 +0530

    Update Header.js

--- src/Component/Header.js
+++ src/Component/Header.js
@@ -8,9 +8,14 @@ function Header() {
     <div>
       <Navbar bg='dark' variant='dark' expand='lg'>
         <Container>
-    <img src={img} style={{ width: "3em", height: "3em",marginRight: 5, borderRadius: "1em" }} alt="">
-          </img>
-          <Navbar.Brand href='#home'>Promote Any Brand</Navbar.Brand>
+    <Link to='/home'>
+     <Link to='/'>
+          <img src={img} style={{ width: "3em", height: "3em",marginRight: 5, borderRadius: "1em" }}>
+            </img>
+          </Link>
+          <Link to='/' style={{ textDecorationLine: "none" }}>
+            <Navbar.Brand>Promote Any Brand</Navbar.Brand>
+          </Link>
           <Navbar.Toggle aria-controls='basic-navbar-nav' />
           <Navbar.Collapse id='basic-navbar-nav'>
             <Nav className='me-auto'>
